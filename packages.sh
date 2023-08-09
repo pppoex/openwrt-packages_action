f2a4086f1b4fead69226ede0964f1ee4df13f6ee
pppoex <120616381+pppoex@users.noreply.github.com>
2023-08-09 20:06:16 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -6,7 +6,7 @@ svn co https://github.com/xiaorouji/openwrt-passwall -b luci # passwall
 svn co https://github.com/siwind/luci-app-wolplus # 网络唤醒++
 svn co https://github.com/sirpdboy/luci-app-advanced # 高级设置
 svn co https://github.com/sirpdboy/luci-app-ddns-go # DDNS-GO
-svn co https://github.com/sirpdboy/luci-app-netwizard # 快捷设置
+svn co https://github.com/kiddin9/luci-app-wizard # 快捷设置
 svn co https://github.com/jerrykuku/luci-app-argon-config # Argon主题设置
 svn co https://github.com/jerrykuku/luci-theme-argon # Argon主题
 svn co https://github.com/openwrt-xiaomi/luci-app-cpufreq # CPU性能调节
