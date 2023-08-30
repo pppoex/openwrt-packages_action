d10ffd4955bb4d4aa440b492d9774645e92b4dd5
pppoex <120616381+pppoex@users.noreply.github.com>
2023-08-30 14:46:58 +0800

    Create build.py

---
+++ build.py
@@ -0,0 +1,22 @@
+import os
+import sys
+#枚举所有Makefile文件位置，存入变量
+def get_makefile_path(root_path):
+    makefile_path_list = []
+    for root, dirs, files in os.walk(root_path):
+        for file in files:
+            if file == "Makefile":
+                makefile_path_list.append(os.path.join(root, file))
+    return makefile_path_list
+
+print ("PATH=" + str(get_makefile_path("./package/")))
+
+# 存入变量
+Makefile_patch = []
+Makefile_patch = get_makefile_path("./package/")
+
+# 遍历所有Makefile文件,在文件位置执行 make [文件位置]/complie V=99
+for i in Makefile_patch:
+    print("make " + i + "/complie V=99")
+    os.system("make " + i + "/complie V=99")
+
